b4631d72b9cb1dfa42c7ba79cc96f9a454ee5799
Moises Joshua <moisesjoshua53@gmail.com>
2025-04-03 08:45:41 -0500

    reminder

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from app.model.calendar import Calendar
 from app.view.console import ConsoleView
 
-
 def main():
     console = ConsoleView()
     console.app_loop()
-
 
 if __name__ == "__main__":
     main()
